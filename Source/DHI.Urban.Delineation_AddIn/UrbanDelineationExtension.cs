5397ce0155bd52c6201873c80f280e852670a6a2
Arnold Engelmann <ahe@dhigroup.com>
2017-01-27 09:08:42 -0800

    implemented OnLoad

--- Source/DHI.Urban.Delineation_AddIn/UrbanDelineationExtension.cs
+++ Source/DHI.Urban.Delineation_AddIn/UrbanDelineationExtension.cs
@@ -25,6 +25,7 @@ using System.Runtime.InteropServices;
 using ESRI.ArcGIS.ADF.Serialization;
 using ESRI.ArcGIS.ArcMapUI;
 using ESRI.ArcGIS.DataSourcesRaster;
+using ESRI.ArcGIS.esriSystem;
 using ESRI.ArcGIS.Framework;
 using ESRI.ArcGIS.Geodatabase;
 
@@ -181,8 +182,71 @@ namespace DHI.Urban.Delineation
 
     protected override void OnLoad(Stream inStrm)
     {
+      SetupOp setupOp = null;
+      try
+      {
+        // NOTE: Do not close or dispose BinaryReader, as this will close the Stream
+        BinaryReader reader = new BinaryReader(inStrm);
+        int version = reader.ReadInt32();
 
-#warning Not Implemented
+        // NOTE: With change to Add-In architecture, this extension is NOT backwards compatible with versions 1 and 2
+
+        if (version == 3)
+        {
+          //// Version 3:
+          //// Item 1: FeatureClassName: The orphan junction feature class of the Geometric Network for the underground conveyance system.
+          //// Item 2: FeatureClassName: The inlet feature class
+          //// Item 3: RasterDatasetName: The DEM dataset
+          //// Item 4: Boolean: Whether to smooth boundaries
+          //// Item 5: Boolean: Whether to include upstream pipe ends
+          //// Item 6: Boolean: Whether to exclude downstream pipe ends
+          //// Item 7: RasterDatasetName: The flow direction dataset
+          //// Item 8: FeatureClassName: The catchment feature class
+          //// Item 9: Boolean: Whether to exclude disabled nodes
+
+          setupOp = new SetupOp();
+          setupOp.ResultsDirectory = this.MxDocDirectoryName;
+          setupOp.ScratchDirectory = this.TemporaryDirectory;
+
+          FeatureClassName networkJunctionName = null;
+          PersistenceHelper.Load<FeatureClassName>(inStrm, ref networkJunctionName);
+          INetworkClass junctionClass = _SafeOpen(networkJunctionName) as INetworkClass;
+          if (junctionClass != null)
+            setupOp.GeometricNetwork = junctionClass.GeometricNetwork;
+
+          FeatureClassName inletClassName = null;
+          PersistenceHelper.Load<FeatureClassName>(inStrm, ref inletClassName);
+          setupOp.InletClass = _SafeOpen(inletClassName) as IFeatureClass;
+
+          RasterDatasetName demName = null;
+          PersistenceHelper.Load<RasterDatasetName>(inStrm, ref demName);
+          IRasterDataset demDataset = _SafeOpen(demName) as IRasterDataset;
+          if (demDataset != null)
+            setupOp.DEM = demDataset.CreateDefaultRaster();
+
+          setupOp.SmoothBoundaries = reader.ReadBoolean();
+          setupOp.IncludeUpstreamPipeEnds = reader.ReadBoolean();
+          setupOp.ExcludeDownstreamPipeEnds = reader.ReadBoolean();
+
+          RasterDatasetName flowDirName = null;
+          PersistenceHelper.Load<RasterDatasetName>(inStrm, ref flowDirName);
+          IRasterDataset flowDirDataset = _SafeOpen(flowDirName) as IRasterDataset;
+          if (flowDirDataset != null)
+            setupOp.FlowDirection = flowDirDataset.CreateDefaultRaster();
+
+          FeatureClassName catchmentClassName = null;
+          PersistenceHelper.Load<FeatureClassName>(inStrm, ref catchmentClassName);
+          setupOp.Catchments = _SafeOpen(catchmentClassName) as IFeatureClass;
+
+          setupOp.ExcludeDisabledNodes = reader.ReadBoolean();
+        }
+
+        _setupOp = setupOp;
+      }
+      catch (Exception ex)
+      {
+        System.Diagnostics.Debug.WriteLine(ex.GetType().FullName + ": " + ex.Message);
+      }
     }
 
     protected override void OnSave(Stream outStrm)
@@ -194,7 +258,7 @@ namespace DHI.Urban.Delineation
 
       // ************** VERSION 3 **************
 
-      // Item 1: The Geometric Network for the underground conveyance system.
+      //// Item 1: FeatureClassName: The orphan junction feature class of the Geometric Network for the underground conveyance system.
       FeatureClassName networkDataset = null;
       if (_setupOp.GeometricNetwork != null)
       {
@@ -203,7 +267,7 @@ namespace DHI.Urban.Delineation
 
       PersistenceHelper.Save<FeatureClassName>(outStrm, networkDataset);
 
-      // Item 2: Inlet featureclass
+      //// Item 2: FeatureClassName: The inlet feature class
       FeatureClassName inletClass = null;
       if (_setupOp.InletClass != null)
       {
@@ -212,7 +276,7 @@ namespace DHI.Urban.Delineation
 
       PersistenceHelper.Save<FeatureClassName>(outStrm, inletClass);
 
-      // Item 3: DEM dataset
+      //// Item 3: RasterDatasetName: The DEM dataset
       RasterDatasetName demDataset = null;
       if (_setupOp.DEM != null)
       {
@@ -225,16 +289,16 @@ namespace DHI.Urban.Delineation
 
       PersistenceHelper.Save<RasterDatasetName>(outStrm, demDataset);
 
-      // Item 4: Whether to smooth boundaries
+      //// Item 4: Boolean: Whether to smooth boundaries
       writer.Write(_setupOp.SmoothBoundaries);
 
-      // Item 5: Whether to include upstream pipe ends
+      //// Item 5: Boolean: Whether to include upstream pipe ends
       writer.Write(_setupOp.IncludeUpstreamPipeEnds);
 
-      // Item 6: Whether to exclude downstream pipe ends
+      //// Item 6: Boolean: Whether to exclude downstream pipe ends
       writer.Write(_setupOp.ExcludeDownstreamPipeEnds);
 
-      // Item 7: Flow direction dataset
+      //// Item 7: RasterDatasetName: The flow direction dataset
       RasterDatasetName flowDirDataset = null;
       if (_setupOp.FlowDirection != null)
       {
@@ -247,7 +311,7 @@ namespace DHI.Urban.Delineation
 
       PersistenceHelper.Save<RasterDatasetName>(outStrm, flowDirDataset);
 
-      // Item 8: Catchment feature class
+      //// Item 8: FeatureClassName: The catchment feature class
       FeatureClassName catchmentClass = null;
       if (_setupOp.Catchments != null)
       {
@@ -256,8 +320,27 @@ namespace DHI.Urban.Delineation
 
       PersistenceHelper.Save<FeatureClassName>(outStrm, catchmentClass);
 
-      // Item 9: Whether to exclude disabled nodes
+      //// Item 9: Boolean: Whether to exclude disabled nodes
       writer.Write(_setupOp.ExcludeDisabledNodes);
+    }
+
+    private object _SafeOpen(IName datasetName)
+    {
+      // Returns null if opening dataset fails (e.g. dataset no longer exists)
+      object result = null;
+      if (datasetName != null)
+      {
+        try
+        {
+          result = datasetName.Open();
+        }
+        catch (COMException comex)
+        {
+          // Do nothing
+          System.Diagnostics.Debug.WriteLine(string.Format("{0}: {1} ({2})", comex.GetType().FullName, comex.Message, comex.ErrorCode));
+        }
+      }
+      return result;
     }
   }
 }
